c5d9734d1a5603174603c4e534460d566e492e4c
Melissa <Melissa@172.16.52.136>
2017-03-16 11:17:52 +0100

    Kontaktformular fertig gestellt.

--- SecondHand/src/java/de/dhbw/my2hand/servlet/MailApp.java
+++ SecondHand/src/java/de/dhbw/my2hand/servlet/MailApp.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.dhbw.my2hand.servlet;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import de.dhbw.my2hand.servlet.SendMail;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,45 +11,47 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "MailApp", urlPatterns = {"/MailApp"})
+
 public class MailApp extends HttpServlet {
+
     private Gson gson = new GsonBuilder().create();
     JsonStatus antwort = new JsonStatus();
-    
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-// Sonderzeichen richtig erkennen!
+        // Sonderzeichen richtig erkennen!
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
-        
-        PrintWriter toBrowser = response.getWriter();
-        
+
+        response.setContentType("application/json");        // SONST ERKENNT DER BROWSER NICHT
+        PrintWriter toBrowser = response.getWriter();       // DASS WIR IHM JSON SCHICKEN!
+
         String action = request.getParameter("action");
         if (action == null) {
             action = "";
         }
 
         switch (action) {
-            
             case "sendContact":
                 String to = "uemran.celen@gmx.de";
                 String name = request.getParameter("name");
                 String subject = request.getParameter("subject");
-                String message =  "Gesendet von: " + name + " \n Nachricht: " + request.getParameter("message");
+                String message = "Gesendet von: " + name + " \n Nachricht: " + request.getParameter("message");
                 String user = "uemran.celen@gmail.com";
                 String pass = "mailiese";
-                SendMail.send(to,subject, message, user, pass);
+                SendMail.send(to, subject, message, user, pass);
                 antwort.status = true;
-                
+
                 gson.toJson(antwort, toBrowser);
                 toBrowser.flush();
-                
+
                 break;
-        
         }
-    }   
+    }
 }
 
 class JsonStatus {
+
     public boolean status;
 }
--- SecondHand/web/email.js
+++ SecondHand/web/email.js
@@ -1,18 +1,40 @@
 function sendContactMail() {
-    
     var name = document.getElementsByName("name")[0].value;
     var subject = document.getElementsByName("subject")[0].value;
     var message = document.getElementsByName("message")[0].value;
-
     var ajax = new XMLHttpRequest();
+
+    if (name === "" || subject === "" || message === "") {
+        document.getElementsByClassName("modal-header")[3].innerHTML =
+                "<button type='button' class='close' data-dismiss='modal'>&times;</button>"
+                + "<h4 class='modal-title'>Kontaktformular</h4>"
+                + "<br><div class='alert alert-danger'>Bitte füllen Sie alle Pflichtfelder aus!</div>";
+        return;
+    }
+
+    ajax.responseType = "json";
     ajax.open("POST", "MailApp?action=sendContact&name=" + encodeURI(name)
             + "&subject=" + encodeURI(subject) + "&message=" + encodeURI(message), true);
     ajax.send();
     ajax.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            if(ajax.response.antwort) {
-                document.getElementsByName("kontaktformular")[0].innerHTML = 
-                "<br><textarea>Mail versendet!</textarea>";
+            console.log(ajax.response);
+            if (ajax.response.status) {
+                $('#contact').modal('hide');
+
+                var x = document.getElementById("snackbar");
+                x.className = "show";
+                // After 3 seconds, remove the show class from DIV
+                setTimeout(function () {
+                    x.className = x.className.replace("show", "");
+                }, 3000);
+
+                document.getElementsByClassName("modal-header")[3].innerHTML =
+                        "<button type='button' class='close' data-dismiss='modal'>&times;</button>"
+                        + "<h4 class='modal-title'>Kontaktformular</h4>";
+                document.getElementsByName("name")[0].value = "";
+                document.getElementsByName("subject")[0].value = "";
+                document.getElementsByName("message")[0].value = "";
             }
         }
     };
